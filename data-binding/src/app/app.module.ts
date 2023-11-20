c334f4bd5cd8be637d40efc2baa8be36230a487f
Fernando Fink <fernando-fink@procergs.rs.gov.br>
2023-11-20 11:06:32 -0300

    Two-way Data Binding

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -4,15 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MeuFormModule } from './meu-form/meu-form.module';
+import { MeuFormComponent } from './meu-form/meu-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataBindingComponent
+    DataBindingComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    FormsModule,
+    MeuFormModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
